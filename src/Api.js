6572fb844f32a7254967d74c7e936944fad724d5
alena <evstafienko_a@ukr.net>
2023-02-23 23:54:13 +0200

    Api

--- src/Api.js
+++ src/Api.js
@@ -1,2 +1,23 @@
+// import axios from 'axios';
 
-const KAY = "eQWUcNgFaIDYHbnDQlu6iYoRZnT3fMvo";
+// const KAY = "eQWUcNgFaIDYHbnDQlu6iYoRZnT3fMvo";
+// const date = new Date();
+// // format YYYY-MM-DD
+
+// axios.defaults.baseURL = 'https://api.apilayer.com/fixer/';
+
+// const Api = async (url = ``) => {
+//   try {
+//     const response = await axios.get(url);
+//     return response;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// export const ApiConvert = () => {
+//   return Api(`convert?base=USD&symbols=EUR,GBP,JPY&amount=5&date=${date}}`);
+// };
+
+
+
